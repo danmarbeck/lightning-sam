precomputedembeddings model now working; fixed focal loss and averaging over masks for losses

model.py
@@ -1,3 +1,5 @@
+from itertools import chain
+
 import torch.nn as nn
 import torch.nn.functional as F
 from segment_anything import sam_model_registry
@@ -58,32 +60,11 @@ class Model(nn.Module):
     def get_predictor(self):
         return SamPredictor(self.model)
 
+    def get_img_size(self):
+        return self.model.image_encoder.img_size
 
-class PrecomputeModel(nn.Module):
-    """
-        Use this model to precompute image embeddings on your dataset for faster training.
-    """
-
-    def __init__(self, cfg):
-        super().__init__()
-        self.cfg = cfg
-
-    def setup(self):
-        self.model = sam_model_registry[self.cfg.model.type](checkpoint=self.cfg.model.checkpoint)
-        self.model.eval()
-        for param in self.model.image_encoder.parameters():
-            param.requires_grad = False
-        for param in self.model.prompt_encoder.parameters():
-            param.requires_grad = False
-        for param in self.model.mask_decoder.parameters():
-            param.requires_grad = False
-
-    def forward(self, images):
-        image_embeddings = self.model.image_encoder(images)
-        return image_embeddings
-
-    def get_predictor(self):
-        return SamPredictor(self.model)
+    def get_parameters(self):
+        return self.model.parameters()
 
 
 class PrecomputedEmbeddingModel(nn.Module):
@@ -145,3 +126,41 @@ class PrecomputedEmbeddingModel(nn.Module):
 
     def get_predictor(self):
         return SamPredictor(self.full_model)
+
+    def get_img_size(self):
+        return self.full_model.image_encoder.img_size
+
+    def get_parameters(self):
+        return chain(*[self.prompt_encoder.parameters(), self.mask_decoder.parameters()])
+
+
+class PrecomputeModel(nn.Module):
+    """
+        Use this model to precompute image embeddings on your dataset for faster training.
+    """
+
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+
+    def setup(self):
+        self.model = sam_model_registry[self.cfg.model.type](checkpoint=self.cfg.model.checkpoint)
+        self.model.eval()
+        for param in self.model.image_encoder.parameters():
+            param.requires_grad = False
+        for param in self.model.prompt_encoder.parameters():
+            param.requires_grad = False
+        for param in self.model.mask_decoder.parameters():
+            param.requires_grad = False
+
+    def forward(self, images):
+        image_embeddings = self.model.image_encoder(images)
+        return image_embeddings
+
+    def get_predictor(self):
+        return SamPredictor(self.model)
+
+
+MODELS = {"PrecomputedEmbeddingModel": PrecomputedEmbeddingModel,
+          "Model": Model,
+          }

precompute_embeddings.py
@@ -18,7 +18,7 @@ def main(cfg: Box) -> None:
     model.setup()
     model = model.to(device=torch.device("cuda:0"))
 
-    train_data, val_data = load_datasets(cfg, model.model.image_encoder.img_size, return_path=True)
+    train_data, val_data = load_datasets(cfg, model.get_img_size(), return_path=True)
 
     model.eval()
 

train.py
@@ -14,7 +14,7 @@ from lightning.fabric.fabric import _FabricOptimizer
 from lightning.fabric.loggers import TensorBoardLogger
 from losses import DiceLoss
 from losses import FocalLoss
-from model import Model
+from model import MODELS, Model
 from torch.utils.data import DataLoader
 from utils import AverageMeter
 from utils import calc_iou
@@ -98,8 +98,8 @@ def train_sam(
             loss_iou = torch.tensor(0., device=fabric.device)
             for pred_mask, gt_mask, iou_prediction in zip(pred_masks, gt_masks, iou_predictions):
                 batch_iou = calc_iou(pred_mask, gt_mask)
-                loss_focal += focal_loss(pred_mask, gt_mask, num_masks)
-                loss_dice += dice_loss(pred_mask, gt_mask, num_masks)
+                loss_focal += focal_loss(pred_mask, gt_mask) / num_masks
+                loss_dice += dice_loss(pred_mask, gt_mask) / num_masks
                 loss_iou += F.mse_loss(iou_prediction, batch_iou, reduction='sum') / num_masks
 
             loss_total = 20. * loss_focal + loss_dice + loss_iou
@@ -139,7 +139,7 @@ def train_sam(
                          }, step=epoch * (iter + 1))
 
 
-def configure_opt(cfg: Box, model: Model):
+def configure_opt(cfg: Box, model):
 
     def lr_lambda(step):
         if step < cfg.opt.warmup_steps:
@@ -151,7 +151,7 @@ def configure_opt(cfg: Box, model: Model):
         else:
             return 1 / (cfg.opt.decay_factor**2)
 
-    optimizer = torch.optim.Adam(model.model.parameters(), lr=cfg.opt.learning_rate, weight_decay=cfg.opt.weight_decay)
+    optimizer = torch.optim.AdamW(model.get_parameters(), lr=cfg.opt.learning_rate, weight_decay=cfg.opt.weight_decay)
     scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda)
 
     return optimizer, scheduler
@@ -170,10 +170,10 @@ def main(cfg: Box) -> None:
         os.makedirs(cfg.out_dir, exist_ok=True)
 
     with fabric.device:
-        model = Model(cfg)
+        model = MODELS[cfg.model.name](cfg)
         model.setup()
 
-    train_data, val_data = load_datasets(cfg, model.model.image_encoder.img_size)
+    train_data, val_data = load_datasets(cfg, model.get_img_size())
     train_data = fabric._setup_dataloader(train_data)
     val_data = fabric._setup_dataloader(val_data)
 