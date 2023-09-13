3203cb33b6bc8d11a842c1d02d348322d8c19797
Daniel Beckmann <d_beck26@uni-muenster.de>
2023-09-13 17:25:04 +0200

    inference now working properly, tested with gaze

--- inference.py
+++ inference.py
@@ -58,7 +58,6 @@ def main(cfg: Box) -> None:
     model = MODELS[cfg.model.name](cfg, inference=True)
     model.setup()
     model = model.to(device=torch.device("cuda:0"))
-    predictor = SamPredictor(model)
 
     transform = ResizeAndPad(model.get_img_size())
     dataset_cls = DATASETS[cfg.dataset.type]
@@ -76,17 +75,16 @@ def main(cfg: Box) -> None:
     with torch.no_grad():
 
         for iter, data in tqdm(enumerate(val), total=len(val)):
-            image, prompt_input, gt_masks, image_path, (H, W), (og_image, og_points, og_bboxes, og_mask_input) = data
+            image, prompt_input, gt_masks, image_path, (H, W), (og_image, og_points, og_bboxes, og_mask_input, padding) = data
             image = image.to(device=device)[None, ...]
             gt_masks = gt_masks.to(device=device)
-
-            predictor.set_torch_image(image, (H, W))
 
             point_coords = prompt_input["points"]
             point_labels = None
             if point_coords is not None:
+                point_coords, point_labels = point_coords
                 point_coords = point_coords.to(device=device)
-                point_labels = torch.ones(point_coords.shape[0:2]).to(device=device)
+                point_labels = point_labels.to(device=device)
             boxes = prompt_input["boxes"]
             if boxes is not None:
                 boxes = boxes.to(device=device)
@@ -94,32 +92,39 @@ def main(cfg: Box) -> None:
             if mask_input is not None:
                 mask_input = mask_input.to(device=device)
 
-            masks, scores, low_res_masks = predictor.predict_torch(
-                point_coords=point_coords,
-                point_labels=point_labels,
-                boxes=boxes,
-                mask_input=mask_input,
-                multimask_output=False,
-            )
-            # masks shape Bx1xHxW
+            prompt_input = {"points": (point_coords, point_labels) if point_coords is not None else None,
+                            "boxes": boxes,
+                            "masks": mask_input}
 
-            val_masks = F.interpolate(
-                low_res_masks,
-                (model.get_img_size(), model.get_img_size()),
-                mode="bilinear",
-                align_corners=False,
-            ).squeeze(1)
+            pred_masks, iou_pred = model(image, (prompt_input,))
 
+            # list with one tensor to tensor
+            pred_masks = pred_masks[0]
+            pred_masks = pred_masks.sigmoid()
             batch_stats = smp.metrics.get_stats(
-                val_masks,
-                gt_masks.int(),
+                pred_masks.unsqueeze(0),
+                gt_masks.int().unsqueeze(0),
                 mode='binary',
                 threshold=0.5,
             )
+
             batch_iou = smp.metrics.iou_score(*batch_stats, reduction="micro-imagewise")
             batch_f1 = smp.metrics.f1_score(*batch_stats, reduction="micro-imagewise")
             ious.update(batch_iou)
             f1_scores.update(batch_f1)
+
+            # unpad
+            pred_masks = pred_masks[..., padding[1]:None if padding[3] == 0 else -padding[3],
+                                       padding[0]:None if padding[2] == 0 else -padding[2]]
+
+            masks = F.interpolate(
+                pred_masks.unsqueeze(0),
+                (H, W),
+                mode="bilinear",
+                align_corners=False,
+            ).squeeze(0)
+
+            masks = masks > 0.5
 
             print(f'Val: Mean IoU: [{ious.avg:.4f}] -- Mean F1: [{f1_scores.avg:.4f}]')
 
@@ -127,7 +132,7 @@ def main(cfg: Box) -> None:
             ax.imshow(og_image)
 
             for pred_idx in range(len(masks)):
-                show_mask(masks[pred_idx, 0].detach().cpu().numpy(), ax=ax, random_color=True)
+                show_mask(masks[pred_idx].detach().cpu().numpy(), ax=ax, random_color=True)
                 if point_coords is not None:
                     show_points(og_points[pred_idx], point_labels[pred_idx].detach().cpu().numpy(), ax=ax,)
                 if mask_input is not None:
